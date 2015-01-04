9407dc13c71172832b6f1ac54bf7d771fa35b2ae
James Stephenson <c4p7.fl1n7@gmail.com>
2015-01-03 20:25:15 -0500

    asciiboard can now render pieces

--- asciiboard.py
+++ asciiboard.py
@@ -21,18 +21,58 @@ for square in renderorder:
     if rowcounter % 8 == 0:
         rowfliper = not rowfliper
 
-def printboard():
-    print("\n\n\n")
-    eightcounter = 0
+def printboard(piecelist):
     rankstr = ""
     for square in renderorder:
-        if square in lightsquares:
-            rankstr += '='
-        else:
-            rankstr += '-'
-        eightcounter += 1
-        if eightcounter % 8 == 0:
-            print(rankstr)
-            rankstr = ""
+        emptysquare = True
+        for piece in piecelist:
+            if square == piece[0]:
+                rankstr += piece[1]
+                emptysquare = False
+                if len(rankstr) == 8:
+                    print(rankstr)
+                    rankstr = ""
+        if emptysquare:
+            if square in lightsquares:
+                rankstr += '='
+            else:
+                rankstr += '-'
+            if len(rankstr) == 8:
+                print(rankstr)
+                rankstr = ""
 
-printboard()
+initialposition = [
+    ("a1", "R"),
+    ("b1", "N"),
+    ("c1", "B"),
+    ("d1", "Q"),
+    ("e1", "K"),
+    ("f1", "B"),
+    ("g1", "N"),
+    ("h1", "R"),
+    ("a8", "r"),
+    ("b8", "n"),
+    ("c8", "b"),
+    ("d8", "q"),
+    ("e8", "k"),
+    ("f8", "b"),
+    ("g8", "n"),
+    ("h8", "r"),
+    ("a2", "P"),
+    ("b2", "P"),
+    ("c2", "P"),
+    ("d2", "P"),
+    ("e2", "P"),
+    ("f2", "P"),
+    ("g2", "P"),
+    ("h2", "P"),
+    ("a7", "p"),
+    ("b7", "p"),
+    ("c7", "p"),
+    ("d7", "p"),
+    ("e7", "p"),
+    ("f7", "p"),
+    ("g7", "p"),
+    ("h7", "p")]
+
+printboard(initialposition)
